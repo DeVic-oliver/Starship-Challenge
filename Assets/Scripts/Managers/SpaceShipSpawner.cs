63c938ee396a9311ec034ed4f78eecf0dcb8750b
Victor Matheus <victormatheus183@gmail.com>
2023-03-25 17:05:28 -0300

    Pure fibonacci commented

--- Assets/Scripts/Managers/SpaceShipSpawner.cs
+++ Assets/Scripts/Managers/SpaceShipSpawner.cs
@@ -16,12 +16,31 @@ public class SpaceShipSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetFibonacci(10);
         GetFibonacciByCoroutine(8);
     }
 
 
-    private void GetFibonacci(int n)
+    //private void GetFibonacci(int n)
+    //{
+    //    int term1 = 0;
+    //    int term2 = 1;
+    //    int result = term1 + term2;
+    //    GetFibonacciSum(term1, term2, result);
+    //}
+
+    //private void GetFibonacciSum(long n1, long n2, long nextTerm){
+    //    countFibbonacci++;
+    //    IntantiateByFibbonacciValue(nextTerm);
+    //    if (countFibbonacci < _fibbonacci)
+    //    {
+    //        long result = n1 + n2; 
+    //        n1 = n2;
+    //        n2 = result;
+    //        GetFibonacciSum(n1, n2, result);
+    //    }
+    //}
+
+
     private void GetFibonacciByCoroutine(int n)
     {
         int term1 = 0;
