c8e2c1163025deaf0daea2f5f20136289fb1d722
Victor Matheus <victormatheus183@gmail.com>
2023-03-25 15:43:43 -0300

    Fibonacci spawner
    Spawns game objects accordingly current fibonacci term value

--- Assets/Scripts/Managers/SpaceShipSpawner.cs
+++ Assets/Scripts/Managers/SpaceShipSpawner.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Assets.Scripts.Service;
 
+
+//Fibonacci Spawner
 public class SpaceShipSpawner : MonoBehaviour
 {
 
@@ -15,29 +16,27 @@ public class SpaceShipSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(Fibonacci.Fibbonacci(10));
-        //Fibbonacci(_fibbonacci);
+        GetFibonacci(10);
     }
 
 
-    private void Fibbonacci(int n)
+    private void GetFibonacci(int n)
     {
         int term1 = 0;
         int term2 = 1;
         int result = term1 + term2;
-        Debug.Log(result);
-        FibbonacciAux(term1, term2, result);
+        GetFibonacciSum(term1, term2, result);
     }
 
-    private void FibbonacciAux(long n1, long n2, long nextTerm){
+    private void GetFibonacciSum(long n1, long n2, long nextTerm){
         countFibbonacci++;
-        if(countFibbonacci < _fibbonacci)
+        IntantiateByFibbonacciValue(nextTerm);
+        if (countFibbonacci < _fibbonacci)
         {
             long result = n1 + n2; 
             n1 = n2;
             n2 = result;
-            Debug.Log(result);
-            FibbonacciAux(n1, n2, result);
+            GetFibonacciSum(n1, n2, result);
         }
     }
 
@@ -46,8 +45,7 @@ public class SpaceShipSpawner : MonoBehaviour
         if(quantity > 0)
         {
             quantity--;
-            //Instantiate(_spaceShip);
-            //Debug.Log($"Space Ship ID: {quantity}");
+            Instantiate(_spaceShip);
             IntantiateByFibbonacciValue(quantity);
         }
     }
