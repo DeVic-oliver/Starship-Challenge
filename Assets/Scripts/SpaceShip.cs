8a9a672a3dbd50896d0356a7a5a9142b4b1c71f3
Victor Matheus <victormatheus183@gmail.com>
2023-03-25 17:04:23 -0300

    SpaceShip coroutines
    Invokes its coroutines to disable it and increment speed when get enabled on game

--- Assets/Scripts/SpaceShip.cs
+++ Assets/Scripts/SpaceShip.cs
@@ -10,39 +10,52 @@ public class SpaceShip : MonoBehaviour
     [Tooltip("A random number between those limits will be the speed")]
     [SerializeField] private float _max = 50f;
     private float _currentSpeed = 0;
+    
+    [Space(15f)]
     [SerializeField] private float _speedIncrement = 1.25f;
 
-    private float _delayToIncrementSpeed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutineToDisable();
+        ResetCurrentSpeed();
+        InitCoroutines();
     }
 
     private void OnEnable()
     {
-        StartCoroutineToDisable();
+        ResetCurrentSpeed();
+        InitCoroutines();
     }
 
-    private void StartCoroutineToDisable()
+    private void ResetCurrentSpeed()
+    {
         _currentSpeed = Random.Range(_min, _max);
+    }
+
+    private void InitCoroutines()
     {
         StartCoroutine("BeginCountdownToDisable");
+        StartCoroutine("IncreaseSpeedByTime");
     }
 
     private IEnumerator BeginCountdownToDisable()
     {
-        yield return new WaitForSeconds(_delayToIncrementSpeed);
+        float delayToDisable = 1f;
+        yield return new WaitForSeconds(delayToDisable);
         gameObject.SetActive(false);
     }
 
+    private IEnumerator IncreaseSpeedByTime()
+    {
         float _delayToIncrementSpeed = 0.1f;
         while (gameObject.activeSelf)
         {
             _currentSpeed += _speedIncrement;
             yield return new WaitForSeconds(_delayToIncrementSpeed);
         }
+    }
+
     private void OnDisable()
     {
         
@@ -51,7 +64,6 @@ public class SpaceShip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         transform.position = transform.position + Vector3.forward * Time.deltaTime * _currentSpeed;
     }
 }
